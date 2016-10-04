b90fa7c8e07759252bf92ab9449cf142d6924d2e
Marc Assens <marc.a.r95@gmail.com>
2016-10-04 16:24:00 +0200

    Uploading files to AWS CloudFront
    Files uploaded to Heroku don't last for long. Heroku
    automatically deletes them. Now files are uploaded to
    a free tier of AWS CloudFront!

---
+++ app/assets/javascripts/nodes.js
@@ -0,0 +1,52 @@
+(function() {
+  var createStorageKey, host, uploadAttachment;
+
+  document.addEventListener("trix-attachment-add", function(event) {
+    var attachment;
+    attachment = event.attachment;
+    if (attachment.file) {
+      return uploadAttachment(attachment);
+    }
+  });
+
+  host = "https://d2j9cjqq2rac4j.cloudfront.net/";
+
+  uploadAttachment = function(attachment) {
+    var file, form, key, xhr;
+    file = attachment.file;
+    key = createStorageKey(file);
+    form = new FormData;
+    form.append("key", key);
+    form.append("Content-Type", file.type);
+    form.append("file", file);
+    xhr = new XMLHttpRequest;
+    xhr.open("POST", host, true);
+
+    xhr.upload.onprogress = function(event) {
+      var progress;
+      progress = event.loaded / event.total * 100;
+      return attachment.setUploadProgress(progress);
+    };
+    
+    xhr.onload = function() {
+      var href, url;
+      if (xhr.status === 204) {
+        url = href = host + key;
+        return attachment.setAttributes({
+          url: url,
+          href: href
+        });
+      }
+    };
+    return xhr.send(form);
+  };
+
+  createStorageKey = function(file) {
+    var date, day, time;
+    date = new Date();
+    day = date.toISOString().slice(0, 10);
+    time = date.getTime();
+    return "tmp/" + day + "/" + time + "-" + file.name;
+  };
+
+}).call(this);
